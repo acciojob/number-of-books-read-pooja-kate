d55dc7cae7a9a257195024c2bdb208c4e55a7ac7
Pooja Kate <82284439+pooja-kate@users.noreply.github.com>
2023-08-14 12:29:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,11 +17,15 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-	let count=0;
-	if(library.readingStatus==true){
-		count++;
-	}
-	return count;
+	let count = 0;
+  
+  for (let i = 0; i < library.length; i++) {
+    if (library[i].readingStatus === true) {
+      count++;
+    }
+  }
+  
+  return count;
 	
   // write your code here
 };
